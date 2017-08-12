cf3e62468b0f357d2974d2d9d129fff635e80f8e
cp <chris@cpsharp.net>
2017-08-12 18:15:47 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The After School Zoo</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+
+<!-- Start of Header -->
+<header>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+
+
+    <div class="container-fluid text-center vertical-center">
+
+        <img src="kitty.gif"
+             class="" alt="Animated calm kitten" title="Meowsome">
+        <h1>The After School Zoo</h1>
+        <h4>Coming soon!</h4>
+    </div>
+</header>
+<!-- End of Header -->
+
+</body>
+</html>
